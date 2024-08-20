9a0539b27caf7ab1a093f29ef564e24f029358f2
TBMODS <61624005+mixserrm999@users.noreply.github.com>
2024-08-20 18:46:08 +0800

    Update setup_alias.sh

--- setup_alias.sh
+++ setup_alias.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # เพิ่ม alias ลงใน .bashrc
-echo "alias deletefile='rm /home/sermixsummoner/SERMIX /home/sermixsummoner/theos/vendor/templates/SERMIX.MOD.MENU.v0.1.0.nic.tar'" >> ~/.bashrc
-# รีโหลด .bashrc เพื่อให้ alias ใช้งานได้ทันที
-source ~/.bashrc
+echo "alias d='rm /home/sermixsummoner/theos/vendor/templates/SERMIX.MOD.MENU.v0.1.0.nic.tar'" >> ~/.bashrc
+echo "alias dd='rm -r /home/sermixsummoner/SERMIX'" >> ~/.bashrc
 
-echo "Alias 'deletefile' ได้ถูกเพิ่มและ .bashrc ได้ถูกรีโหลดแล้ว."
+# แจ้งเตือนให้รัน source ~/.bashrc
+echo "Alias 'd' และ 'dd' ได้ถูกเพิ่มลงใน .bashrc แล้ว."
+echo "กรุณารัน 'source ~/.bashrc' หรือเปิด shell ใหม่เพื่อให้ alias มีผล."
