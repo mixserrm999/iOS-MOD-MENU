b9e54df6372647e216f434c603ec7aaf5651338d
TBMODS <61624005+mixserrm999@users.noreply.github.com>
2024-08-20 19:17:52 +0800

    Update set_sdk.sh

--- set_sdk.sh
+++ set_sdk.sh
@@ -4,7 +4,6 @@
 DOWNLOAD_URL="https://github.com/xybp888/iOS-SDKs/releases/download/iOS-SDKs/iPhoneOS13.6.sdk.zip"
 DEST_DIR="/home/sermixsummoner/theos/sdks"
 ZIP_FILE="${DEST_DIR}/iPhoneOS13.6.sdk.zip"
-EXTRACTED_DIR="${DEST_DIR}/iPhoneOS13.6.sdk"
 
 # สร้างไดเรกทอรีปลายทางถ้ายังไม่มี
 mkdir -p "$DEST_DIR"
@@ -18,7 +17,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# แตกไฟล์ zip
+# แตกไฟล์ zip ไปยังไดเรกทอรีที่กำหนด
 unzip -o "$ZIP_FILE" -d "$DEST_DIR"
 
 # ตรวจสอบการแตกไฟล์
@@ -27,17 +26,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# เปลี่ยนชื่อไดเรกทอรีที่แตกไฟล์
-mv "${DEST_DIR}/iPhoneOS13.6.sdk" "$EXTRACTED_DIR"
-
-# ตรวจสอบการเปลี่ยนชื่อ
-if [ $? -eq 0 ]; then
-    echo "File downloaded and extracted successfully."
-else
-    echo "Error renaming the directory."
-    exit 1
-fi
-
 # ลบไฟล์ zip ถ้าต้องการ
 rm "$ZIP_FILE"
 
+echo "File downloaded and extracted successfully."
